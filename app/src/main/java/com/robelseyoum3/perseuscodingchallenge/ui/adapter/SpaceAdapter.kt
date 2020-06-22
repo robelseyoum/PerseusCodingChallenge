f8a6eb20ee9c2546a2af2943abc42b48abc9eabe
Robel <robelseyoum3@gmail.com>
2020-06-22 15:41:29 +0100

    adding human readable duration and time format

--- app/src/main/java/com/robelseyoum3/perseuscodingchallenge/ui/adapter/SpaceAdapter.kt
+++ app/src/main/java/com/robelseyoum3/perseuscodingchallenge/ui/adapter/SpaceAdapter.kt
@@ -1,12 +1,15 @@
 package com.robelseyoum3.perseuscodingchallenge.ui.adapter
 
+import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.robelseyoum3.perseuscodingchallenge.R
-import com.robelseyoum3.perseuscodingchallenge.data.model.isspasstimes.OpenNotify
 import com.robelseyoum3.perseuscodingchallenge.data.model.isspasstimes.Response
 import com.robelseyoum3.perseuscodingchallenge.ui.adapter.viewholder.SpaceViewHolder
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class SpaceAdapter constructor(val notify: MutableList<Response>): RecyclerView.Adapter<SpaceViewHolder>(){
 
@@ -16,8 +19,19 @@ class SpaceAdapter constructor(val notify: MutableList<Response>): RecyclerView.
     override fun getItemCount() =  notify.size
 
     override fun onBindViewHolder(holder: SpaceViewHolder, position: Int) {
-        holder.tvDuration.text = notify[position].duration.toString()
-        holder.tvRiseTime.text = notify[position].risetime.toString()
+        holder.tvDuration.text = formatDuration(notify[position].duration.toLong())
+        holder.tvRiseTime.text = getDateString(notify[position].risetime.toLong())
     }
+
+    private val simpleDateFormat = SimpleDateFormat("dd MMMM yyyy, HH:mm:ss", Locale.ENGLISH)
+
+    private fun getDateString(time: Long) : String = simpleDateFormat.format(time * 1000L)
+
+    private fun formatDuration(seconds: Long): String = if (seconds < 60) {
+        seconds.toString()
+    } else {
+        DateUtils.formatElapsedTime(seconds)
+    }
+
 
 }
